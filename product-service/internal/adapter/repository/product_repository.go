f78527ff91af6cdc55ae0cfe9d3cd5e494c1f0af
Jonathan Gunawan <jgunawan3005@gmail.com>
2026-04-06 15:53:47 +0700

    fix(product-service): downgrade elasticsearch client from v9 to v8

--- product-service/internal/adapter/repository/product_repository.go
+++ product-service/internal/adapter/repository/product_repository.go
@@ -10,9 +10,9 @@ import (
 	"product-service/utils/message"
 	"strings"
 
-	"github.com/elastic/go-elasticsearch/v9"
-	"github.com/elastic/go-elasticsearch/v9/typedapi/types"
-	"github.com/elastic/go-elasticsearch/v9/typedapi/types/enums/sortorder"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/sortorder"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
